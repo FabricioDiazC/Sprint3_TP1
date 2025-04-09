9a7739a8015c7430b15ba368ac62b8ddb686816c
Fabri <diazcornejofabricio@gmail.com>
2025-04-08 22:01:51 -0300

    TP3 Sprint 3

---
+++ services/superheroesService.mjs
@@ -0,0 +1,50 @@
+import superHeroRepository from '../repositories/SuperHeroRepository.mjs';
+
+export async function obtenerSuperheroePorId(id){
+    return await superHeroRepository.obtenerPorId(id);
+}
+
+export async function obtenerTodosLosSuperheroes(){
+    return await superHeroRepository.obtenerTodos();
+}
+
+export async function buscarSuperheroesPorAtributo(atributo, valor) {
+    return await superHeroRepository.buscarPorAtributo(atributo, valor);
+}
+
+export async function obtenerSuperheroesMayoresDe30() {
+    return await superHeroRepository.obtenerMayoresDe30();
+}
+
+//Sprint 3 TP1
+//Lo que hice yo
+/*
+export async function agregarSuperheroeNuevo(){
+    return await superHeroRepository.agregarSuperheroeNuevo();
+}
+    */
+   
+//Lo que hizo Chat GPT
+export async function crearSuperheroe(datosSuperheroe) {
+    return await superHeroRepository.crearSuperheroe(datosSuperheroe);
+}
+
+//Actualizar superheroe por id
+export async function actualizarSuperheroe(id, datosActualizados) {
+    return await superHeroRepository.actualizarSuperheroe(id, datosActualizados);
+}
+
+//Borrar a un Superheroe por ID
+export async function eliminarSuperheroe(id) {
+    return await superHeroRepository.eliminarSuperheroe(id);
+}
+
+//Borrar un Superheroe por Nombre
+/*
+export async function eliminarSuperheroePorNombre(nombre) {
+    return await superHeroRepository.eliminarSuperheroePorNombre(nombre);
+}
+*/
+export async function borrarSuperheroPorNombre(nombre) {
+    return await superHeroRepository.borrarSuperheroPorNombre(nombre)
+  }
